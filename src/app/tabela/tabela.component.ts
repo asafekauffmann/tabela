d2ccc3cd533f236997e1f35a2716c972f406aa22
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-12-08 11:46:15 -0300

    up

--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
    selector: 'app-tabela',
    templateUrl: './tabela.component.html',
    styleUrls: ['./tabela.component.scss']
 })
-export class TabelaComponent implements OnInit {
-   @Input() dados: Array<{ id: number, paciente: string, recepcao: string }> | null = null;
+export class TabelaComponent implements OnChanges {
 
-   constructor() { }
+   colunas: Array<string> = [];
+   @Input() dados: Array<any> = [];
+   /* { id: number, paciente: string, recepcao: string } */
+   
+   constructor() {
 
-   ngOnInit(): void {
+   }
+
+   ngOnChanges(changes: SimpleChanges) {
+      if(changes.dados) {
+         const primeiroItem = this.dados[0];
+         this.colunas = Object.getOwnPropertyNames(primeiroItem);
+
+         console.log(this.colunas);
+      }
    }
 
 }
