ae23ed7e0875ae7dd4d91b358361785872ffb130
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-12-08 11:30:15 -0300

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tabela';
+   dados: Array<{ id: number, paciente: string, recepcao: string }> = [];
+
+   constructor() {
+      this.dados = [
+         {
+            "id": 1,
+            "paciente": "Martin",
+            "recepcao": "12/06/2021"
+         },
+         {
+            "id": 2,
+            "paciente": "Lenny",
+            "recepcao": "17/05/2020"
+         }
+      ];
+   };
+
 }
--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tabela',
-  templateUrl: './tabela.component.html',
-  styleUrls: ['./tabela.component.scss']
+   selector: 'app-tabela',
+   templateUrl: './tabela.component.html',
+   styleUrls: ['./tabela.component.scss']
 })
 export class TabelaComponent implements OnInit {
+   @Input() dados: Array<{ id: number, paciente: string, recepcao: string }> | null = null;
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+   }
 
 }
